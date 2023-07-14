10bc7a47fc79237f024cd64d153acfef86261964
vipulkc28 <75108786+vipulkc28@users.noreply.github.com>
2023-07-14 17:01:45 +0530

    Update App.js

--- RasaUI/src/App.js
+++ RasaUI/src/App.js
@@ -5,8 +5,19 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 import Basic from "./components/Basic";
+import { useEffect } from 'react';
+
 
 function App() {
+  useEffect(() => {
+    window.addEventListener('message', handleMessage);
+  }, []);
+
+  const handleMessage = event => {
+    if (event.origin.includes('cmnetwork.co')) {
+      window.userDetails = event.userData;
+    }
+  };
   return (
     <div className="App">
       <Basic />
